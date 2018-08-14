1742224b049ebbd1f79a026d0296935728a28147
jpierce <jd.pierce@promega.com>
2018-08-14 10:31:48 -0700

    now setting pixel to red

--- OctoShirtSketch/OctoShirtSketch.ino
+++ OctoShirtSketch/OctoShirtSketch.ino
@@ -1,11 +1,22 @@
 #include <Adafruit_NeoPixel.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define PIN 5
+#define COUNT 1
 
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(COUNT, PIN, NEO_GRB + NEO_KHZ800);
+
+int currentDelay = 500; // delay for half a second
+
+void setup()
+{
+  pixels.begin(); // This initializes the NeoPixel library.
+  return;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  pixels.setPixelColor(0, pixels.Color(150,0,0));
+  pixels.show();
+  
+  delay(currentDelay);
 }
